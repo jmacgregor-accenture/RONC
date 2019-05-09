bc2b8f17aa2aed9dce8c35e1b61b0df926ac8f4a
John MacGregor <jmacgregor@pillartechnology.com>
2019-05-09 11:40:17 -0400

    Added conditional setting of gecko driver name

--- RONCFrontEnd/RONCFrontEnd.IntegrationTests/EndToEndTests.cs
+++ RONCFrontEnd/RONCFrontEnd.IntegrationTests/EndToEndTests.cs
@@ -10,11 +10,27 @@ namespace RONCFrontEnd.IntegrationTests
 {
     public class EndToEndTests
     {
+        private string _driverFile = SetGeckoDriver();
+        
+        private static string SetGeckoDriver()
+        {
+            var variable = Environment.GetEnvironmentVariable("GeckoDriverFile");
+
+            if (variable != null)
+            {
+                return variable;
+            } 
+            
+            return "geckodriver";
+            
+            
+        }
+        
         [Fact]
         public void HomePageIsCorrect()
         {
             // TODO something here, possibly the way we're getting the Environment.CurrentDirectory and the hardcoded result, is throwing a "System.ComponentModel.Win32Exception; exec format error"
-            var service = FirefoxDriverService.CreateDefaultService(Environment.CurrentDirectory, "geckodriverlinux");   
+            var service = FirefoxDriverService.CreateDefaultService(Environment.CurrentDirectory, _driverFile);   
             var firefoxOptions = new FirefoxOptions();
             firefoxOptions.AddArgument("--headless");
             var webdriver = new FirefoxDriver(service,firefoxOptions);
